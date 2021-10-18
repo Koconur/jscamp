4c6163dfd6c2b216f1617fc8998157f88c5a05d7
Koconur <koconur@outlook.com>
2021-10-19 00:26:18 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun=11 const sonrası bu tanımlama hata verir
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Kredisi","Özel Krediler"]
+
+console.log("<ul>")
+for (let index = 0; index <konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
